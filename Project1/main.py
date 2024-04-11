a674e799514746a6f6bdb8f38c90ec01540ac515
SarahBader <145606559+BaderSarah@users.noreply.github.com>
2024-04-11 15:31:45 +0200

    Update main.py

--- Project1/main.py
+++ Project1/main.py
@@ -1,1 +1,17 @@
+import random
 
+
+def binary_numbers_conversions():
+    number = random.randint()
+    print(f"represent {number} in binary")
+
+
+def binary_numbers_addition(): ...
+
+
+def main():
+    binary_numbers_conversions()
+
+
+if __name__ == "__main__":
+    main()
