3a7cc8c5c66b2581e98425886f69c07d013a2249
BaderSarah <bader.sarah@icloud.com>
2024-04-11 16:58:01 +0200

    second commit

--- Project1/main.py
+++ Project1/main.py
@@ -8,14 +8,24 @@ def binary_numbers_conversions():
     if result == "0b" + answer:
         print("Your answer is correct!")
     else:
-        print(f"Wrong answer, the correct answer is {result}")
+        print(f"Wrong answer, the correct answer is {result.replace("b", "0")}.")
 
 
-def binary_numbers_addition(): ...
+def binary_numbers_addition(): 
+    first_number = random.randint(0,256)
+    second_number = random.randint(0,256)
+    answer = input(f"{bin(first_number).replace("b", "0")} \
++ {bin(second_number).replace("b", "0")} = ")
+    result = bin(first_number + second_number).replace("b", "0")
+    if result == answer:
+        print("Your answer is correct!")
+    else:
+        print(f"Wrong answer, the correct answer is {result}.")
 
 
 def main():
     binary_numbers_conversions()
+    binary_numbers_addition()
 
 
 if __name__ == "__main__":
