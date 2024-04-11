0535a15fb865b3172d90c821954f41031c4966ba
BaderSarah <bader.sarah@icloud.com>
2024-04-11 16:33:19 +0200

    first commit

--- Project1/main.py
+++ Project1/main.py
@@ -2,8 +2,13 @@ import random
 
 
 def binary_numbers_conversions():
-    number = random.randint()
-    print(f"represent {number} in binary")
+    number = random.randint(0, 256)
+    answer = input(f"represent {number} in binary (start with the first 1): ")
+    result = bin(number)
+    if result == "0b" + answer:
+        print("Your answer is correct!")
+    else:
+        print(f"Wrong answer, the correct answer is {result}")
 
 
 def binary_numbers_addition(): ...
