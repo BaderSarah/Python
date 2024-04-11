0e1fc58f4ca23edc1fbdb3d95a8022c74435607c
BaderSarah <bader.sarah@icloud.com>
2024-04-11 21:06:15 +0200

    adding two new functions in project 1

--- Project1/main.py
+++ Project1/main.py
@@ -9,24 +9,48 @@ def binary_numbers_conversions():
         print("Your answer is correct!")
     else:
         print(f"Wrong answer, the correct answer is \
-{result.replace("b", "0")}.")
+{result[2:]}.")
 
 
 def binary_numbers_addition(): 
     first_number = random.randint(0, 256)
     second_number = random.randint(0, 256)
-    answer = input(f"{bin(first_number).replace("b", "0")} \
-+ {bin(second_number).replace("b", "0")} = ")
-    result = bin(first_number + second_number).replace("b", "0")
+    answer = input(f"{bin(first_number)[2:]} \
++ {bin(second_number)[2:]} = ")
+    result = bin(first_number + second_number)
     if result == answer:
         print("Your answer is correct!")
     else:
-        print(f"Wrong answer, the correct answer is {result}.")
+        print(f"Wrong answer, the correct answer is {result[2:]}.")
+
+
+def hex_numbers_conversions(): 
+    number = random.randint(0, 256)
+    answer = input(f"represent {number} in hexadecimal: ")
+    result = hex(number)
+    if result == answer:
+        print("Your answer is correct!")
+    else:
+        print(f"Wrong answer, the correct answer is \
+{result[2:]}.")
+
+
+def octal_numbers_conversions():
+    number = random.randint(0, 256)
+    answer = input(f"represent {number} in octal: ")
+    result = oct(number)
+    if result == answer:
+        print("Your answer is correct!")
+    else:
+        print(f"Wrong answer, the correct answer is \
+{result[2:]}.")
 
 
 def main():
     binary_numbers_conversions()
     binary_numbers_addition()
+    hex_numbers_conversions()
+    octal_numbers_conversions()
 
 
 if __name__ == "__main__":
